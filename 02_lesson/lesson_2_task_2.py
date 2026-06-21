696a7318dea37c8b43b0b90035b2358c08b6bb9b
BadIceMan <Alphabettacappa2@gmail.com>
2026-06-21 19:22:03 +0300

    Фикс задач 2 и 3 по замечаниям наставника

--- 02_lesson/lesson_2_task_2.py
+++ 02_lesson/lesson_2_task_2.py
@@ -1,5 +1,8 @@
 def is_year_leap(year):
-    return year % 4 == 0
+    if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+        return True
+    else:
+        return False
 
 
 current_year = 2024
