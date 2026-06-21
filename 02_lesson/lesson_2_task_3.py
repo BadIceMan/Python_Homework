77e3f2dcaca9e0794b0bb1dfb66584632945332b
BadIceMan <Alphabettacappa2@gmail.com>
2026-06-21 19:22:03 +0300

    Фикс задач 2 и 3 по замечаниям наставника

--- 02_lesson/lesson_2_task_3.py
+++ 02_lesson/lesson_2_task_3.py
@@ -1,9 +1,9 @@
-import math
-
-
 def square(side):
     area = side * side
-    return math.ceil(area)
+    if area % 1 == 0:
+        return int(area)
+    else:
+        return int(area) + 1
 
 
 print(square(5))
